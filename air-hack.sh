6d2764b4e3d71b4a5511cfb676f13b478870da34
SHEHEERALIZX <nssheheerali@gmail.com>
2021-02-26 15:08:29 +0530

    25/02/2021

--- air-hack.sh
+++ air-hack.sh
@@ -1,1 +1,22 @@
-echo "Hello World"
+echo "Happy Hacking"
+echo "Aircrack Automation Tool"
+
+
+iwconfig
+
+
+
+read -p "Enter the adapter name : " adapter
+
+
+
+sudo airmon-ng start $adapter
+
+$mon = "mon"
+
+sudo airodump-ng ${adapter}mon
+
+gnome-terminal
+
+ls
+
